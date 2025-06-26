a1af3f93bc83dbc2c026a719da5da81bdcd28f90
Moritz Drexl <mdrexl@fastmail.fm>
2025-06-26 20:45:47 +0200

    more detailed test

--- vscode/src/test/extension.test.ts
+++ vscode/src/test/extension.test.ts
@@ -1,7 +1,6 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
-import * as path from 'path';
-// import * as zigcount from '../extension';
+import { Position, TextDocument, DiagnosticSeverity } from 'vscode';
 
 suite('LSP', () => {
     let doc: vscode.TextDocument;
@@ -11,48 +10,91 @@ suite('LSP', () => {
     });
 
     test('Hover', async function() {
-        const position = findEndOf('    Assets:Checking', doc);
-        await moveTo(doc, position);
-
-        const hovers = await vscode.commands.executeCommand<vscode.Hover[]>(
-            'vscode.executeHoverProvider',
-            doc.uri,
-            position
-        );
-
-        assert.ok(hovers && hovers.length === 1, 'Expected one hover result');
-        const contents = hovers[0].contents
-            .map(c => (typeof c === 'string' ? c : c.value))
-            .join('\n');
-        assertContains('100.10', contents);
-        assertContains('200.20', contents);
+        await testHover(doc, new Position(10, 4), (contents) => {
+            assertContains('100.10', contents);
+            assertContains('200.20', contents);
+        });
     });
 
     test('Highlight', async function() {
-        const position = findEndOf('    Assets:Checking', doc);
-        await moveTo(doc, position);
+        await testHighlight(doc, new Position(10, 4), [
+            toRange(7, 16, 31),
+            toRange(10, 4, 19),
+            toRange(14, 4, 19),
+            toRange(18, 4, 19),
+            toRange(22, 4, 19),
+            toRange(26, 4, 19),
+        ]);
 
-        const highlights = await vscode.commands.executeCommand<vscode.DocumentHighlight[]>(
-            'vscode.executeDocumentHighlights',
-            doc.uri,
-            position
-        );
+        await testHighlight(doc, new Position(2, 17), [
+            toRange(2, 15, 40),
+        ]);
 
-        assert.equal(highlights.length, 9);
+        await testHighlight(doc, new Position(3, 17), [
+            toRange(3, 16, 39),
+        ]);
     });
 
     test('Diagnostics', async function() {
-        const diagnostics = vscode.languages.getDiagnostics(doc.uri);
-
-        assert.equal(diagnostics.length, 2);
+        testDiagnostics(doc, [
+            {
+                prefix: "Invalid date",
+                range: toRange(29, 0, 10),
+                severity: DiagnosticSeverity.Error
+            },
+            {
+                prefix: "Account is not open",
+                range: toRange(3, 16, 39),
+                severity: DiagnosticSeverity.Warning
+            },
+        ])
     });
 });
+
+async function testHover(doc: TextDocument, pos: Position, check: (contents: string) => void) {
+    await moveTo(doc, pos);
+    const hovers = await vscode.commands.executeCommand<vscode.Hover[]>(
+        'vscode.executeHoverProvider',
+        doc.uri,
+        pos
+    );
+    assert.equal(hovers.length, 1);
+    const contents = hovers[0].contents
+        .map(c => (typeof c === 'string' ? c : c.value))
+        .join('\n');
+    check(contents);
+}
+
+async function testHighlight(doc: TextDocument, pos: Position, expectedRanges: vscode.Range[]) {
+    await moveTo(doc, pos);
+    const actualHighlights = await vscode.commands.executeCommand<vscode.DocumentHighlight[]>(
+        'vscode.executeDocumentHighlights',
+        doc.uri,
+        pos
+    );
+    assert.equal(actualHighlights.length, expectedRanges.length);
+    expectedRanges.forEach((expected, i) => {
+        const actual = actualHighlights[i].range;
+        assert.deepEqual(expected, actual);
+    });
+}
+
+function testDiagnostics(doc: TextDocument, expected: any[]) {
+    const diagnostics = vscode.languages.getDiagnostics(doc.uri);
+    assert.equal(diagnostics.length, expected.length);
+    expected.forEach((e, i) => {
+        const a = diagnostics[i];
+        assert.ok(a.message.startsWith(e.prefix), `"${a.message}" should start with "${e.prefix}"`);
+        assert.deepEqual(e.range, a.range);
+        assert.equal(e.severity, a.severity);
+    });
+}
 
 async function sleep(ms: number = 10) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function openDoc(file: string): Promise<vscode.TextDocument> {
+async function openDoc(file: string): Promise<TextDocument> {
     const root = vscode.workspace.workspaceFolders![0].uri;
     const uri = vscode.Uri.joinPath(root, file);
     const doc = await vscode.workspace.openTextDocument(uri);
@@ -62,7 +104,7 @@ async function openDoc(file: string): Promise<vscode.TextDocument> {
     return doc;
 }
 
-async function moveTo(doc: vscode.TextDocument, position: vscode.Position) {
+async function moveTo(doc: TextDocument, position: Position) {
     await vscode.window.showTextDocument(doc, { selection: new vscode.Range(position, position) });
     await sleep();
 }
@@ -71,10 +113,8 @@ function assertContains(needle: string, haystack: string) {
     assert.ok(haystack.includes(needle), `Expected "${haystack}" to contain "${needle}"`);
 }
 
-function findEndOf(needle: string, doc: vscode.TextDocument): vscode.Position {
-    const lines = doc.getText().split(/\r?\n/);
-    const lineNum = lines.findIndex(l => l.includes(needle));
-    assert.ok(lineNum >= 0, `Unable to find ${needle} in document`);
-    const col = lines[lineNum].indexOf(needle) + needle.length - 1;
-    return new vscode.Position(lineNum, col);
+function toRange(line: number, sChar: number, eChar: number) {
+    const start = new vscode.Position(line, sChar);
+    const end = new vscode.Position(line, eChar);
+    return new vscode.Range(start, end);
 }
