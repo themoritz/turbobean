7f86117db5dec05d5d4fc0ff203fe8b8a84a8cef
Moritz Drexl <mdrexl@fastmail.fm>
2025-06-26 12:23:47 +0200

    tabs -> spaces

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -4,45 +4,45 @@ import { LanguageClientOptions, ServerOptions, LanguageClient, Executable, Trans
 let client: LanguageClient;
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Shit, your extension "vscode" is now active!');
+    console.log('Shit, your extension "vscode" is now active!');
 
-	const exe: Executable = {
-		command: '/Users/moritz/code/zigcount/zig-out/bin/zigcount',
-		args: ['--lsp'],
-		transport: TransportKind.stdio,
-	}
+    const exe: Executable = {
+        command: '/Users/moritz/code/zigcount/zig-out/bin/zigcount',
+        args: ['--lsp'],
+        transport: TransportKind.stdio,
+    }
 
-	const serverOptions: ServerOptions = {
-		run: exe,
-		debug: exe
-	}
+    const serverOptions: ServerOptions = {
+        run: exe,
+        debug: exe
+    }
 
-	// Options to control the language client
-	const clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'beancount' }],
-		synchronize: {
-			fileEvents: vscode.workspace.createFileSystemWatcher('**/.bean')
-		},
-		outputChannel: vscode.window.createOutputChannel('Zigcount'),
-	};
+    // Options to control the language client
+    const clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{ scheme: 'file', language: 'beancount' }],
+        synchronize: {
+            fileEvents: vscode.workspace.createFileSystemWatcher('**/.bean')
+        },
+        outputChannel: vscode.window.createOutputChannel('Zigcount'),
+    };
 
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'zigcount',
-		'zigcount VSCode extension',
-		serverOptions,
-		clientOptions
-	);
+    // Create the language client and start the client.
+    client = new LanguageClient(
+        'zigcount',
+        'zigcount VSCode extension',
+        serverOptions,
+        clientOptions
+    );
 
-	// Start the client. This will also launch the server
-	client.start();
+    // Start the client. This will also launch the server
+    client.start();
 }
 
 // This method is called when your extension is deactivated
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
 }
