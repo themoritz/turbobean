a5d942fc2780b1ffa5799da4d03ae7060ac4f333
Moritz Drexl <mdrexl@fastmail.fm>
2025-08-20 14:35:32 +0200

    journal into its own file

--- src/server.zig
+++ src/server.zig
@@ -4,14 +4,13 @@ const tardy = @import("zzz").tardy;
 const Tardy = tardy.Tardy(.auto);
 const Runtime = @import("zzz").tardy.Runtime;
 const Project = @import("project.zig");
-const Tree = @import("tree.zig");
-const Uri = @import("Uri.zig");
+const journal = @import("server/journal.zig");
 
 const Watch = @import("Watch.zig");
 
 const http = @import("zzz").HTTP;
 
-const ServerState = struct {
+pub const ServerState = struct {
     project: *Project,
     watch: *Watch,
 };
@@ -41,7 +40,7 @@ pub fn run(alloc: std.mem.Allocator, project: *Project) !void {
     var router = try http.Router.init(alloc, &.{
         http.Route.init("/").get({}, index_handler).layer(),
         http.Route.init("/journal").get({}, index_handler).layer(),
-        http.Route.init("/sse/journal").get(&state, journal_handler).layer(),
+        http.Route.init("/sse/journal").get(&state, journal.handler).layer(),
         http.FsDir.serve("/static", static_dir),
     }, .{});
     defer router.deinit(alloc);
@@ -83,99 +82,4 @@ fn index_handler(ctx: *const http.Context, _: void) !http.Respond {
         .body = body.items,
         .mime = http.Mime.HTML,
     });
-}
-
-const JournalParams = struct {
-    account: []const u8,
-};
-
-fn journal_handler(ctx: *const http.Context, state: *ServerState) !http.Respond {
-    var sse = try http.SSE.init(ctx);
-
-    const params = try http.Query(JournalParams).parse(ctx.allocator, ctx);
-
-    var body = std.ArrayList(u8).init(ctx.allocator);
-    const out = body.writer();
-
-    var listener = try state.watch.newListener(ctx.runtime);
-    defer listener.deinit();
-
-    while (true) {
-        try render_journal(ctx.allocator, state.project, params.account, out.any());
-
-        sse.send(.{ .data = body.items }) catch |err| switch (err) {
-            error.Closed => {
-                std.log.debug("Client closed.", .{});
-                break;
-            },
-            else => return err,
-        };
-
-        body.clearRetainingCapacity();
-
-        const path = listener.awaitChanged();
-        const uri = try Uri.from_absolute(ctx.allocator, path);
-        const source = try uri.load_nullterminated(state.project.alloc);
-        try state.project.update_file(uri.value, source);
-    }
-
-    return .responded;
-}
-
-fn render_journal(
-    alloc: std.mem.Allocator,
-    project: *Project,
-    account: []const u8,
-    out: std.io.AnyWriter,
-) !void {
-    const t = @embedFile("templates/journal.html");
-
-    var tree = try Tree.init(alloc);
-
-    try zts.write(t, "table", out);
-
-    for (project.sorted_entries.items) |sorted_entry| {
-        const data = project.files.items[sorted_entry.file];
-        const entry = data.entries.items[sorted_entry.entry];
-        switch (entry.payload) {
-            .open => |open| {
-                if (std.mem.eql(u8, open.account.slice, account)) {
-                    _ = try tree.open(open.account.slice, null, open.booking_method);
-                    try zts.print(t, "open_row", .{
-                        .date = entry.date,
-                    }, out);
-                }
-            },
-            .transaction => |tx| {
-                if (tx.postings) |postings| {
-                    for (postings.start..postings.end) |i| {
-                        const p = data.postings.get(i);
-                        if (std.mem.eql(u8, p.account.slice, account)) {
-                            try zts.print(t, "tx_row", .{
-                                .date = entry.date,
-                                .flag = tx.flag.slice,
-                                .payee = tx.payee,
-                                .narration = tx.narration,
-                                .change_units = p.amount.number.?,
-                                .change_cur = p.amount.currency.?,
-                            }, out);
-                            try tree.postInventory(entry.date, p);
-                            var sum = try tree.inventoryAggregatedByAccount(alloc, account);
-                            var iter = sum.by_currency.iterator();
-                            while (iter.next()) |kv| {
-                                try zts.print(t, "balance_cur", .{
-                                    .units = kv.value_ptr.total_units(),
-                                    .cur = kv.key_ptr.*,
-                                }, out);
-                            }
-                            try zts.write(t, "end_tx_row", out);
-                        }
-                    }
-                }
-            },
-            else => {},
-        }
-    }
-
-    try zts.write(t, "end_table", out);
 }
---
+++ src/server/journal.zig
@@ -0,0 +1,102 @@
+const std = @import("std");
+const zts = @import("zts");
+const http = @import("zzz").HTTP;
+const ServerState = @import("../server.zig").ServerState;
+const Uri = @import("../Uri.zig");
+const Project = @import("../project.zig");
+const Tree = @import("../tree.zig");
+
+const Params = struct {
+    account: []const u8,
+};
+
+pub fn handler(ctx: *const http.Context, state: *ServerState) !http.Respond {
+    var sse = try http.SSE.init(ctx);
+
+    const params = try http.Query(Params).parse(ctx.allocator, ctx);
+
+    var body = std.ArrayList(u8).init(ctx.allocator);
+    const out = body.writer();
+
+    var listener = try state.watch.newListener(ctx.runtime);
+    defer listener.deinit();
+
+    while (true) {
+        try render(ctx.allocator, state.project, params.account, out.any());
+
+        sse.send(.{ .data = body.items }) catch |err| switch (err) {
+            error.Closed => {
+                std.log.debug("Client closed.", .{});
+                break;
+            },
+            else => return err,
+        };
+
+        body.clearRetainingCapacity();
+
+        const path = listener.awaitChanged();
+        const uri = try Uri.from_absolute(ctx.allocator, path);
+        const source = try uri.load_nullterminated(state.project.alloc);
+        try state.project.update_file(uri.value, source);
+    }
+
+    return .responded;
+}
+
+fn render(
+    alloc: std.mem.Allocator,
+    project: *Project,
+    account: []const u8,
+    out: std.io.AnyWriter,
+) !void {
+    const t = @embedFile("../templates/journal.html");
+
+    var tree = try Tree.init(alloc);
+
+    try zts.write(t, "table", out);
+
+    for (project.sorted_entries.items) |sorted_entry| {
+        const data = project.files.items[sorted_entry.file];
+        const entry = data.entries.items[sorted_entry.entry];
+        switch (entry.payload) {
+            .open => |open| {
+                if (std.mem.eql(u8, open.account.slice, account)) {
+                    _ = try tree.open(open.account.slice, null, open.booking_method);
+                    try zts.print(t, "open_row", .{
+                        .date = entry.date,
+                    }, out);
+                }
+            },
+            .transaction => |tx| {
+                if (tx.postings) |postings| {
+                    for (postings.start..postings.end) |i| {
+                        const p = data.postings.get(i);
+                        if (std.mem.eql(u8, p.account.slice, account)) {
+                            try zts.print(t, "tx_row", .{
+                                .date = entry.date,
+                                .flag = tx.flag.slice,
+                                .payee = tx.payee,
+                                .narration = tx.narration,
+                                .change_units = p.amount.number.?,
+                                .change_cur = p.amount.currency.?,
+                            }, out);
+                            try tree.postInventory(entry.date, p);
+                            var sum = try tree.inventoryAggregatedByAccount(alloc, account);
+                            var iter = sum.by_currency.iterator();
+                            while (iter.next()) |kv| {
+                                try zts.print(t, "balance_cur", .{
+                                    .units = kv.value_ptr.total_units(),
+                                    .cur = kv.key_ptr.*,
+                                }, out);
+                            }
+                            try zts.write(t, "end_tx_row", out);
+                        }
+                    }
+                }
+            },
+            else => {},
+        }
+    }
+
+    try zts.write(t, "end_table", out);
+}
