e1d4e274991189d857897ead916b79695b497424
Moritz Drexl <mdrexl@fastmail.fm>
2026-03-17 10:00:06 +1300

    add fmt subcommand

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,9 @@ const Data = @import("data.zig");
 const parser = @import("parser.zig");
 const Project = @import("project.zig");
 const Uri = @import("Uri.zig");
+const Ast = @import("Ast.zig");
+const AstRender = @import("AstRender.zig");
+const ErrorDetails = @import("ErrorDetails.zig");
 
 const lsp = @import("lsp.zig");
 const server = @import("server.zig");
@@ -98,6 +101,36 @@ pub fn main() !void {
                 return;
             }
         }
+        if (std.mem.eql(u8, command, "fmt")) {
+            const file_arg = iter.next();
+            const source = if (file_arg) |file| blk: {
+                const f = try std.fs.cwd().openFile(file, .{});
+                defer f.close();
+                break :blk try f.readToEndAllocOptions(alloc, std.math.maxInt(usize), null, .@"1", 0);
+            } else blk: {
+                break :blk try std.fs.File.stdin().readToEndAllocOptions(alloc, std.math.maxInt(usize), null, .@"1", 0);
+            };
+            defer alloc.free(source);
+
+            const uri = Uri{ .value = file_arg orelse "<stdin>" };
+            var ast = try Ast.parse(alloc, uri, source);
+            defer ast.deinit();
+
+            if (ast.errors.items.len > 0) {
+                var stderr_buf: [4096]u8 = undefined;
+                var stderr_w = std.fs.File.stderr().writer(&stderr_buf);
+                for (ast.errors.items) |err| {
+                    try err.format(&stderr_w.interface, alloc, true);
+                }
+                try stderr_w.interface.flush();
+                std.process.exit(1);
+            }
+
+            var stdout_buf: [4096]u8 = undefined;
+            var stdout_w = std.fs.File.stdout().writer(&stdout_buf);
+            try AstRender.render(alloc, &stdout_w.interface, &ast);
+            return;
+        }
         if (std.mem.eql(u8, command, "-h")) {
             cli.printHelp();
             return;
@@ -119,6 +152,8 @@ test {
     _ = @import("tree.zig");
     _ = @import("ErrorDetails.zig");
     _ = @import("Uri.zig");
+    _ = Ast;
+    _ = AstRender;
     _ = @import("solver.zig");
     _ = @import("server/DisplaySettings.zig");
     _ = semantic_tokens;
